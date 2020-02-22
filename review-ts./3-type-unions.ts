9b37f2d1bae8bb9af07d387f2e6a772c48ebb24e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-22 01:35:49 -0800

    Create 3-type-unions.ts

---
+++ review-ts./3-type-unions.ts
@@ -0,0 +1,15 @@
+let a: string | Boolean;
+
+a = "Charles";
+a = true;
+a = 232
+
+console.log(a)
+//error will b triggered off  during compilation 
+
+
+let names: string | string [];
+names = "Marcy"
+console.log(names)
+names = [ 'Marcy', 'alice', 'venera']
+console.log(names)
