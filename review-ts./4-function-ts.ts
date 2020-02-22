b96f895456c4110c8a0e703033a8b0d197807228
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-22 02:22:15 -0800

    Create 4-function-ts.ts

---
+++ review-ts./4-function-ts.ts
@@ -0,0 +1,14 @@
+//:string is return type
+function myFunction (param: string) : string {
+  return "This is TS"
+}
+
+console.log(myFunction())
+
+
+// //return is not allowed
+// function myFunction1 (param: string) : void {
+//   return "This is TS"
+// }
+
+// console.log(myFunction1())
